26da2125b7eaabee3625b63db85080c39be695eb
Marvin Hagemeister <hello@marvinh.dev>
2020-10-15 11:07:54 +0200

    Add rough support for template strings

--- src/lexer/string.test.ts
+++ src/lexer/string.test.ts
@@ -16,6 +16,12 @@ function expectString(code: string, str: string) {
 	expect(lexer.string).toEqual(str);
 }
 
+function expectTemplate(code: string, str: string) {
+	const lexer = lex(code);
+	expect(lexer.token).toEqual(Token.TemplateHead);
+	expect(lexer.string).toEqual(str);
+}
+
 describe("Lex StringLiteral", () => {
 	it("should parse simple string literals", () => {
 		expectString("'bar'", "bar");
@@ -28,5 +34,15 @@ describe("Lex StringLiteral", () => {
 
 	it("should throw on newline characters", () => {
 		expect(() => lex('"foo\nbar"')).toThrowError(SyntaxError as any);
+	});
+
+	describe("Template Strings", () => {
+		it("should parse template string", () => {
+			expectTemplate("`foo`", "foo");
+		});
+
+		it("should only parse template start", () => {
+			expectTemplate("`foo${1}`", "foo");
+		});
 	});
 });
--- src/lexer/string.ts
+++ src/lexer/string.ts
@@ -1,4 +1,4 @@
-import { expectToken, Lexer, step } from "../lexer";
+import { Lexer, step } from "../lexer";
 import { CharFlags } from "../lexer-ascii";
 import { Char } from "../lexer_helpers";
 import { Token } from "../tokens";
@@ -6,24 +6,42 @@ import { Token } from "../tokens";
 export function scanStringLiteral(lexer: Lexer) {
 	const quote = lexer.char;
 	// TODO: Template literals
-	lexer.token = Token.StringLiteral;
+	lexer.token =
+		quote === Char.Backtick ? Token.TemplateHead : Token.StringLiteral;
 	step(lexer);
 
+	let suffixLen = 0;
 	// TODO: Add support for escaped codes
 	// TODO: Unicode
 	while (lexer.char !== quote && lexer.char !== Char.EndOfFile) {
-		if ((lexer.flags & CharFlags.NewLine) === CharFlags.NewLine) {
+		if (
+			quote !== Char.Backtick &&
+			(lexer.flags & CharFlags.NewLine) === CharFlags.NewLine
+		) {
 			throw new SyntaxError(
 				`Unterminated string literal. Newline characters need to be escaped.`
 			);
 		}
 
+		if (lexer.char === Char.$) {
+			if (quote === Char.Backtick) {
+				step(lexer);
+
+				if ((lexer.char as number) === Char["{"]) {
+					lexer.token = Token.TemplateHead;
+					suffixLen = 2;
+					step(lexer);
+					break;
+				}
+			}
+		}
+
 		step(lexer);
 	}
 
-	if (lexer.char !== quote) {
+	if (quote !== Char.Backtick && lexer.char !== quote) {
 		throw new SyntaxError("Unterminated string literal");
 	}
 
-	lexer.string = lexer.source.slice(lexer.start + 1, lexer.end);
+	lexer.string = lexer.source.slice(lexer.start + 1, lexer.end - suffixLen);
 }
