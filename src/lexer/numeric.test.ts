974c3900e7255464550a4565bdf29ee7d4b07a17
Marvin Hagemeister <hello@marvinh.dev>
2020-10-15 10:41:26 +0200

    Re-format numeric tests

--- src/lexer/numeric.test.ts
+++ src/lexer/numeric.test.ts
@@ -23,50 +23,48 @@ function expectNumber(code: string, n: number) {
 	}
 }
 
-describe.only("Lexer", () => {
-	describe("Numeric", () => {
-		it("should discard leading 0", () => {
-			expectNumber("000", 0);
-			expectNumber("0987", 987);
-			expectNumber("01289", 1289);
-		});
+describe("Lex NumericLiteral", () => {
+	it("should discard leading 0", () => {
+		expectNumber("000", 0);
+		expectNumber("0987", 987);
+		expectNumber("01289", 1289);
+	});
 
-		it("should scan integers", () => {
-			expectNumber("0", 0);
-			expectNumber("123", 123);
-			expectNumber("999999999", 999999999);
-			expectNumber("9999999999", 9999999999);
-			expectNumber("99999999999", 99999999999);
-			expectNumber("123456789123456789", 123456789123456780);
-			expectNumber(
-				"123456789123456789" + "0".repeat(128),
-				1.2345678912345679e145
-			);
-		});
+	it("should scan integers", () => {
+		expectNumber("0", 0);
+		expectNumber("123", 123);
+		expectNumber("999999999", 999999999);
+		expectNumber("9999999999", 9999999999);
+		expectNumber("99999999999", 99999999999);
+		expectNumber("123456789123456789", 123456789123456780);
+		expectNumber(
+			"123456789123456789" + "0".repeat(128),
+			1.2345678912345679e145
+		);
+	});
 
-		it("should scan floats", () => {
-			expectNumber("0987.6543", 987.6543);
-		});
+	it("should scan floats", () => {
+		expectNumber("0987.6543", 987.6543);
+	});
 
-		it.skip("should scan Binary (base2)", () => {
-			// TODO
-		});
-		it.skip("should scan Octal (base8)", () => {
-			// TODO
-		});
-		it.skip("should scan Hex (base16)", () => {
-			// TODO
-		});
+	it.skip("should scan Binary (base2)", () => {
+		// TODO
+	});
+	it.skip("should scan Octal (base8)", () => {
+		// TODO
+	});
+	it.skip("should scan Hex (base16)", () => {
+		// TODO
+	});
 
-		it("should scan with underscore", () => {
-			expectNumber("1_000_000", 1_000_000);
-			expect(() => lex("1__000_000")).toThrowError(SyntaxError as any);
-			expect(() => lex("01_2.2__1")).toThrowError(SyntaxError as any);
-			expect(() => lex("1_")).toThrowError(SyntaxError as any);
-		});
+	it("should scan with underscore", () => {
+		expectNumber("1_000_000", 1_000_000);
+		expect(() => lex("1__000_000")).toThrowError(SyntaxError as any);
+		expect(() => lex("01_2.2__1")).toThrowError(SyntaxError as any);
+		expect(() => lex("1_")).toThrowError(SyntaxError as any);
+	});
 
-		it("should throw if identifier follows immediately after number", () => {
-			expect(() => lex("11abc")).toThrowError(SyntaxError as any);
-		});
+	it("should throw if identifier follows immediately after number", () => {
+		expect(() => lex("11abc")).toThrowError(SyntaxError as any);
 	});
 });
